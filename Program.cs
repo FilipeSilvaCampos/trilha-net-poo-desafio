95388e9f94b299b206c411a2bef62d1b39bbfaac
Filipe Campos <camposfilipe41@gmail.com>
2023-12-10 20:00:48 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("7399988", "Modelo 1", "11111", 64);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone("7399889", "Modelo 2", "00000", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
